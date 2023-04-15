36e15716aa1b1b9772001ff7d7c3ac64ea4512dd
ŁukaszKryczka <60490378+cheatermen@users.noreply.github.com>
2023-04-16 00:40:01 +0200

    Reimagines bottom 25 percentile of unprobable words
    Former-commit-id: 71329a0cd55e72cdf432961acf5d205cd659c7e5

---
+++ src/suggestions/testing.py
@@ -0,0 +1,12 @@
+from suggestionsLibrary import upgradeSentence
+from sys import argv
+
+if __name__ == "__main__":
+    while(True):
+        zdanie = input("Enter sentence: ") if len(argv) == 1 else argv[1]
+        # use quotes to make a sentence a single argument:
+        # python3 getWordProbability.py "To jest zdanie przykładowe"
+        # print("Prompt: ",zdanie)
+        upgradeSentence(zdanie)
+        #print("Upgrade: ",upgradeSentence(zdanie))
+    
