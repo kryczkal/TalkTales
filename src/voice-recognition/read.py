27be092ee38c6497ee4e82a87d65737f1b9495ff
WojciechMat <wmatsad@gmail.com>
2023-04-24 20:35:27 +0200

    lekki bajzel, ale z VAD
    Former-commit-id: 8b9830c016a0d4fc5a35b9596ec1d71bb2d8f66e

--- src/voice-recognition/read.py
+++ src/voice-recognition/read.py
@@ -1,7 +1,7 @@
 import pyaudio
 import numpy as np
 import time as t
-
+from webrtcvad import Vad
 from Sample import Sample
 from Settings import Settings
 
@@ -9,25 +9,29 @@ from plots import plot_mfcc
 import matplotlib.pylab
 
 audio = pyaudio.PyAudio()
-stream = audio.open(format=Settings.FORMAT, channels=Settings.CHANNELS, rate=Settings.FREQUENCY, input=True, frames_per_buffer=Settings.CHUNK_SIZE)
+stream = audio.open(format=Settings.FORMAT, channels=Settings.CHANNELS,
+                    rate=Settings.FREQUENCY, input=True, frames_per_buffer=Settings.CHUNK_SIZE)
 
 try:
     print("Recording audio... Press Ctrl+C to stop.")
     buffer = []
-    
+
     for i in range(100):
-        # if(Vad.is_speech(y)): continue
-        sample = Sample(np.frombuffer(stream.read(Settings.CHUNK_SIZE), dtype=np.float32))
-        sample.get_mfccs()
+        buff = stream.read(Settings.CHUNK_SIZE)
+        frame = np.frombuffer(buff, dtype=np.float32)
+        frame = frame.astype(np.float32, order='C') / 32768.0 #żeby dzialalo z settings dla vad
+
+        sample = Sample(frame)
+        sample.get_mfcc()
         buffer.append(sample)
     for i in range(100):
-        matplotlib.pylab.subplot(10,10,i+1)
+        matplotlib.pylab.subplot(10, 10, i + 1)
         plot_mfcc(buffer[i])
 
     matplotlib.pylab.show()
 
 except KeyboardInterrupt:
-    print("Printing stopped")
+    print("Recording stopped")
 finally:
     stream.stop_stream()
     stream.close()
---
+++ src/voice-recognition/read_with_vad.py
@@ -0,0 +1,53 @@
+import pyaudio
+import numpy as np
+import time as t
+from webrtcvad import Vad
+from Sample import Sample
+from Settings import Settings
+
+from plots import plot_mfcc
+import matplotlib.pylab
+
+audio = pyaudio.PyAudio()
+stream = audio.open(format=Settings.FORMAT, channels=Settings.CHANNELS,
+                    rate=Settings.FREQUENCY, input=True, frames_per_buffer=Settings.CHUNK_SIZE)
+
+try:
+    print("Recording audio... Press Ctrl+C to stop.")
+    buffer = []
+    not_detected = 0
+    num_appended = 0
+    append = True
+    for i in range(0, 200):
+        buff = stream.read(Settings.CHUNK_SIZE)
+        frame = np.frombuffer(buff, dtype=np.int16)
+        frame = frame.astype(np.float32, order='C') / 32768.0
+        vad = Vad()
+
+        if not vad.is_speech(buf=buff, sample_rate=Settings.FREQUENCY):
+            not_detected += 1
+            continue
+        sample = Sample(frame)
+
+        if append:
+            buffer.append(sample)
+            append = False
+        else:
+            buffer[num_appended].concatenate_data(sample)
+            buffer[num_appended].get_mfcc()
+            append = True
+            num_appended += 1
+    print("frames with no voice: ", not_detected)
+    for i in range(0, len(buffer)):
+        matplotlib.pylab.subplot(10, 10, i+1)
+
+        plot_mfcc(buffer[i])
+
+    matplotlib.pylab.show()
+
+except KeyboardInterrupt:
+    print("Recording stopped")
+finally:
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
