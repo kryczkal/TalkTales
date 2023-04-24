2af5d4f9d88375eff536eb396145693d58d632c3
Jakub Lisowski <115403674+Jlisowskyy@users.noreply.github.com>
2023-04-24 19:27:06 +0200

    VAD - testy
    Dodalem vad i zmienilem format na inty, problem z mfcc librosy nie chce liczyc po intach, ale to trzeba jeszcze zweryfikowac
    
    
    Former-commit-id: 444593a49ce9b3b74fae76a51656bf1676348249

--- src/voice-recognition/Sample.py
+++ src/voice-recognition/Sample.py
@@ -1,15 +1,34 @@
 from Settings import Settings
+import numpy as np
 import librosa
 import librosa.feature
 
 class Sample:
-    def __init__(self, data):
-        self.data = data
-        self.mfcc = 0
-        self.mel = 0
 
-    def get_mfccs(self):
-        self.mel = librosa.feature.melspectrogram(y=self.data, sr=Settings.FREQUENCY)
-        self.mfcc = librosa.feature.mfcc(y=self.data, S=self.mel, sr=Settings.FREQUENCY, n_mfcc=13, fmin=100, fmax=8000, lifter=1)
+    def __init__(self, ByteData, IsSpeech, TimeStamp):
+        self.ByteData = ByteData
+        self.IsSpeech = IsSpeech
+        self.TimeStamp = TimeStamp
+        self.Data = None
+        self.Mfcc = None
+        self.Mel = None
 
-    
+    def __is_empty(self) -> bool:
+        # Checks if there is lack of some data
+        return self.Data is None or self.Mel is None
+
+    def GetMfcc(self):
+        # Compute elements if needed
+        if self.__is_empty():
+            self.ConvertData()
+
+        # Compute Mfcc component
+        self.Mfcc = librosa.feature.mfcc(y=self.Data, S=self.Mel, sr=Settings.FREQUENCY,
+                                          n_mfcc=13, fmin=100, fmax=8000, lifter=1,
+                                          n_fft=len(self.Data), dtype=Settings.DATAFORMAT)
+
+    def ConvertData(self):
+        # Convert data from raw byte string and extract necessary information
+        self.Data = np.frombuffer(self.ByteData, dtype=Settings.DATAFORMAT)
+        # self.Mel = librosa.feature.melspectrogram(y=self.Data, sr=Settings.FREQUENCY, dtype=Settings.DATAFORMAT)
+        # TODO Problem jest tutaj jakis
