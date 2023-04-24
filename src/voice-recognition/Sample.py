8b9830c016a0d4fc5a35b9596ec1d71bb2d8f66e
WojciechMat <wmatsad@gmail.com>
2023-04-24 20:35:27 +0200

    lekki bajzel, ale z VAD

--- src/voice-recognition/Sample.py
+++ src/voice-recognition/Sample.py
@@ -1,15 +1,22 @@
+import numpy as np
+
 from Settings import Settings
 import librosa
 import librosa.feature
+import numpy
+
 
 class Sample:
-    def __init__(self, data):
+    def __init__(self, data: np.ndarray):
         self.data = data
         self.mfcc = 0
         self.mel = 0
 
-    def get_mfccs(self):
+    def get_mfcc(self):
         self.mel = librosa.feature.melspectrogram(y=self.data, sr=Settings.FREQUENCY)
         self.mfcc = librosa.feature.mfcc(y=self.data, S=self.mel, sr=Settings.FREQUENCY, n_mfcc=13, fmin=100, fmax=8000, lifter=1)
 
-    
+    def concatenate_data(self, s):
+        self.data = numpy.concatenate((self.data, s.data), axis=None)
+        return self
+
--- src/voice-recognition/Settings.py
+++ src/voice-recognition/Settings.py
@@ -1,9 +1,14 @@
 import pyaudio
 
 class Settings:
-    FORMAT = pyaudio.paFloat32  # Audio format (32-bit floats)
+    FORMAT = pyaudio.paInt16#pa.Float32   # Audio format (32-bit floats)*
     CHANNELS = 1  # Mono
-    FREQUENCY = 44100  # Sampling rate (44.1kHz)
-    SEGMENT_DURATION_MS = 60  # Segment duration in milliseconds
+    FREQUENCY = 48000#44100  # Sampling rate (44.1kHz)*
+    SEGMENT_DURATION_MS = 30 #60  # Segment duration in milliseconds*
     frames_per_segment = int(FREQUENCY * SEGMENT_DURATION_MS / 1000)
     CHUNK_SIZE = frames_per_segment  # Size of each audio chunk
+
+    #* changes for VAD:
+    #pa.Int16
+    #48000
+    #30
