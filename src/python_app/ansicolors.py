6a03f5879c724c7e3269e737877618956cdb89bc
Tomek Mycielski <60585367+Al-Gor1thm@users.noreply.github.com>
2023-05-01 12:53:23 +0200

    Cleaned up rt_test a little; Added linting to my files - I use vanilla flake8
    Former-commit-id: bfcc9d9e2ec2ae8ce5df1301fb92f9b6d4b85af5

--- src/python_app/ansicolors.py
+++ src/python_app/ansicolors.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+
 class Colors(Enum):
     BLACK = 0
     RED = 1
@@ -21,20 +22,23 @@ class Colors(Enum):
     def __int__(self):
         return self.value
 
+
 def reset():
     return '\033[0m'
+
 
 def bold():
     return '\033[1m'
 
+
 def underline():
     return '\033[4m'
+
 
 def inverse():
     return '\033[7m'
 
-def color(fg: Colors|int, bg: Colors|int|None=None):
+
+def color(fg: Colors | int, bg: Colors | int | None = None):
     return f'\033[{30 + (int)(fg) + ((int)(fg) & 8)//2 * 13}' + (
         f';{40 + (int)(bg) + ((int)(bg) & 8)//2 * 13}' if bg else '') + 'm'
-
-
