b30389558c91351605437209612bb598e8dc24d5
wookie <elek123456789101112@gmail.com>
2023-05-04 00:35:35 +0200

    przepraszam ale walczylem z dodaniem commita przez 35min

--- src/voice-recognition/read.py
+++ src/voice-recognition/read.py
@@ -11,42 +11,50 @@ from Settings import Settings
 
 from plots import plot_mfcc
 import matplotlib.pyplot as plt
-from GMM import GMM
+from Gmm import Gmm, gmm_kl
 
 audio = pyaudio.PyAudio()
 stream = audio.open(format=Settings.STREAMFORMAT, channels=Settings.CHANNELS, rate=Settings.FREQUENCY, input=True, frames_per_buffer=Settings.CHUNK_SIZE)
 Vad = webrtcvad.Vad(1)
 
-audio_buffer = []
+speaker_gmm = Gmm()
+hypothetical_gmm = Gmm()
 analyze_buffer = []
-
-gmm_buffer = np.zeros((13, 1))
-Gmm = GMM(np.array(gmm_buffer))
-
+once = True
+it = 200
+#stop = 1
 try:
     print("Recording audio... Press Ctrl+C to stop.")
 
     counter = 0
-
+    analyze_counter = 0
     while True:
         Data = stream.read(Settings.CHUNK_SIZE)
         Temp = Sample(Data,
                       Vad.is_speech(Data, Settings.FREQUENCY),
                       counter * Settings.SEGMENT_DURATION_MS)
-        
+
         counter += 1
 
-
-        audio_buffer.append(Temp)
-
+        if once:
+            mfcc_buffer = Temp.GetMfcc().T
+            once = False
         if Temp.IsSpeech:
             analyze_buffer.append(Temp)
-            gmm_buffer = np.concatenate((gmm_buffer, Temp.GetMfcc()), axis=1)
+            mfcc_buffer = np.append(mfcc_buffer, Temp.GetMfcc().T, axis=0)
 
-            start_time = time.time()
-            model = GMM(np.array(gmm_buffer))
-            end_time = time.time()
-            print("zajelo to:", end_time-start_time)
+            analyze_counter += 1
+            if analyze_counter % it != 0:
+                continue
+
+            speaker_gmm.train(mfcc_buffer)
+            hypothetical_gmm.train(mfcc_buffer[-it:])
+
+            divergence = gmm_kl(speaker_gmm.getModel(), hypothetical_gmm.getModel())
+            print("KL divergence = ", divergence)
+
+            #exit()
+
 
 except KeyboardInterrupt:
     print("Recording stopped")
