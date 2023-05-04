b8f629b211f816a11afcb9b0977bae8c007e6714
wookie <elek123456789101112@gmail.com>
2023-05-04 18:52:52 +0200

    plotowanie divergances po zakonczonym odsluchu

--- src/voice-recognition/read.py
+++ src/voice-recognition/read.py
@@ -19,15 +19,19 @@ Vad = webrtcvad.Vad(1)
 
 speaker_gmm = Gmm()
 hypothetical_gmm = Gmm()
+hypothetical_gmm2 = Gmm()
 analyze_buffer = []
 once = True
-it = 200
+it = 300
+divergances = []
+divergances2 = []
 #stop = 1
 try:
     print("Recording audio... Press Ctrl+C to stop.")
 
     counter = 0
-    analyze_counter = 0
+    analyze_counter = 1
+
     while True:
         Data = stream.read(Settings.CHUNK_SIZE)
         Temp = Sample(Data,
@@ -39,6 +43,11 @@ try:
         if once:
             mfcc_buffer = Temp.GetMfcc().T
             once = False
+
+        if counter < 600 and Temp.IsSpeech:
+            np.append(mfcc_buffer, Temp.GetMfcc().T, axis=0)   
+            continue         
+            
         if Temp.IsSpeech:
             analyze_buffer.append(Temp)
             mfcc_buffer = np.append(mfcc_buffer, Temp.GetMfcc().T, axis=0)
@@ -49,24 +58,20 @@ try:
 
             speaker_gmm.train(mfcc_buffer)
             hypothetical_gmm.train(mfcc_buffer[-it:])
+            hypothetical_gmm2.train(mfcc_buffer[-it*2:-it])
 
             divergence = gmm_kl(speaker_gmm.getModel(), hypothetical_gmm.getModel())
+            divergence2 = gmm_kl(hypothetical_gmm2.getModel(), hypothetical_gmm.getModel())
             print("KL divergence = ", divergence)
-
+            divergances.append(divergence)
+            divergances2.append(divergance2)
             #exit()
 
 
 except KeyboardInterrupt:
     print("Recording stopped")
-
-    analyze_buffer = CreateOverlappedData(analyze_buffer)
-    ExtractInformation(analyze_buffer)
-
-    for Num, Seg in enumerate(analyze_buffer):
-        if Num < 5*7:
-            plt.subplot(5,7,Num+1)
-            plot_mfcc(Seg)
-
+    plt.plot(divergances)
+    plt.plot(divergances2)
     plt.show()
 
 finally:
---
+++ src/voice-recognition/test.py
@@ -0,0 +1,87 @@
+import time
+
+import pyaudio
+import numpy as np
+import time as t
+import webrtcvad
+
+from Sample import Sample
+from VoiceRecog import *
+from Settings import Settings
+
+from plots import plot_mfcc
+import matplotlib.pyplot as plt
+from Gmm import Gmm, gmm_kl
+# from queue import Queue
+
+audio = pyaudio.PyAudio()
+stream = audio.open(format=Settings.STREAMFORMAT, channels=Settings.CHANNELS, rate=Settings.FREQUENCY, input=True, frames_per_buffer=Settings.CHUNK_SIZE)
+Vad = webrtcvad.Vad(1)
+
+speaker_gmm = Gmm()
+hypothetical_gmm = Gmm()
+analyze_buffer = []
+once = True
+it = 200
+#stop = 1
+try:
+    print("Recording audio... Press Ctrl+C to stop.")
+
+    counter = 0
+    analyze_counter = 0
+    while True:
+        Data = stream.read(Settings.CHUNK_SIZE)
+        Temp = Sample(Data,
+                      Vad.is_speech(Data, Settings.FREQUENCY),
+                      counter * Settings.SEGMENT_DURATION_MS)
+
+        counter += 1
+
+        if once:
+            mfcc_buffer = Temp.GetMfcc().T
+            once = False
+
+        if Temp.IsSpeech:
+            analyze_buffer.append(Temp)
+            #mfcc_buffer = np.vstack((mfcc_buffer, Temp.GetMfcc().T))
+            #print("dodajemy:")
+            #print(Temp.GetMfcc().T)
+            mfcc_buffer = np.append(mfcc_buffer, Temp.GetMfcc().T, axis=0)
+            #print("nowa konkatanacja")
+            #print(mfcc_buffer)
+            #if stop % 3 == 0:
+            #    exit()
+            #stop += 1
+            analyze_counter += 1
+            if analyze_counter % it != 0:
+                continue
+
+            speaker_gmm.train(mfcc_buffer)
+            #print(mfcc_buffer.shape)
+            #print("ostatnie", it)
+            #print(mfcc_buffer[-it:].shape)
+            hypothetical_gmm.train(mfcc_buffer[-it:])
+
+            divergence = gmm_kl(speaker_gmm.getModel(), hypothetical_gmm.getModel())
+            print("KL divergence = ", divergence)
+
+            #exit()
+
+
+except KeyboardInterrupt:
+    print("Recording stopped")
+
+    analyze_buffer = CreateOverlappedData(analyze_buffer)
+    ExtractInformation(analyze_buffer)
+
+    for Num, Seg in enumerate(analyze_buffer):
+        if Num < 5*7:
+            plt.subplot(5,7,Num+1)
+            plot_mfcc(Seg)
+
+    plt.show()
+
+finally:
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
