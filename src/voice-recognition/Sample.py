b5a9b112238df1d507686dfe721f270cd250867b
wookie <elek123456789101112@gmail.com>
2023-05-04 23:40:11 +0200

    Dzialajace (Mniej wiecej) speaker recognition. Jest w stanie wczytywac do streamu plik.
    Co-authored-by: Wojciech Matejuk <WojciechMat@users.noreply.github.com>
    Co-authored-by: forzajlisowskyy@gmail.com <forzajlisowskyy@gmail.com>

--- src/voice-recognition/Sample.py
+++ src/voice-recognition/Sample.py
@@ -1,10 +1,8 @@
 import numpy as np
 import librosa
 import librosa.feature
-import webrtcvad
 
 from Settings import Settings
-Vad = webrtcvad.Vad(1)
 
 class VoiceSample:
     def __init__(self, byte_data, is_speech, time_stamp):
@@ -13,20 +11,14 @@ class VoiceSample:
         self.is_speech = is_speech
         self.data = None
         self.mfcc = None
-        self.mel = None
-
-
-    def __is_empty(self) -> bool:
-        # Checks if there is lack of some data
-        return self.data is None or self.mel is None
     
     def mfcc_get(self):
         # Compute elements if needed
-        if self.__is_empty():
+        if self.data is None:
             self.data_convert()
 
         # Compute Mfcc component
-        self.mfcc = librosa.feature.mfcc(y=self.data, S=self.mel, sr=Settings.FREQUENCY,
+        self.mfcc = librosa.feature.mfcc(y=self.data, sr=Settings.FREQUENCY,
                                           n_mfcc=13, fmin=100, fmax=8000, lifter=1,
                                           n_fft=len(self.data))
         return self.mfcc
