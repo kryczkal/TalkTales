0319a3f014b965c87f6cfc5ee275d00cb9fce7f1
wookie <elek123456789101112@gmail.com>
2023-05-06 20:51:53 +0200

    Absolute insanity.
    Parralel testing of 40 models at a time
    
    Co-authored-by: Wojciech Matejuk <WojciechMat@users.noreply.github.com>
    
    Former-commit-id: 4bdd53ae2d58e33b9d743790d84bf1fdf0eb2bef

--- src/voice_recognition/test.py
+++ src/voice_recognition/test.py
@@ -4,6 +4,8 @@ import numpy as np
 import time as t
 import torch
 import torchaudio
+import os
+from tqdm import tqdm
 
 from Sample import VoiceSample
 from Settings import Settings
@@ -12,7 +14,7 @@ from plots import plot_mfcc
 import matplotlib.pyplot as plt
 from Speaker import Speaker, kl_distance
 from Recognizer import Recongnizer
-
+import itertools
 audio = pyaudio.PyAudio()
 
 torchaudio.set_audio_backend("soundfile")
@@ -26,15 +28,28 @@ vad, utils = torch.hub.load(repo_or_dir='snakers4/silero-vad',
  VADIterator,
  collect_chunks) = utils
 
-recognizer = Recongnizer(30)
+rn = np.random.RandomState(26)
+
+recognizers = [Recongnizer(30, 0 + rec_id, number_treshold=10 + i, percentage_treshold=0.20 + j, gmm_is_trained__data_treshold = 1600 - k)\
+                for (rec_id, i, j, k) in zip(range(0, 10), np.arange(0, 30, 3), np.arange(0, 0.20, 0.02), range(0,  1180, 118))] \
++ [Recongnizer(30, 10 + rec_id, number_treshold=13 + i, percentage_treshold=0.35, gmm_is_trained__data_treshold = 1600)\
+                   for rec_id, i in zip(range(0,10), np.arange(0, 30, 3))] \
++ [Recongnizer(30, 20 + rec_id, percentage_treshold=0.37 + i, gmm_is_trained__data_treshold = 1600)\
+                   for rec_id, i in zip(range(0,10), np.arange(0, 0.20, 0.2))] \
++ [Recongnizer(30, 30 + rec_id, gmm_is_trained__data_treshold = 1320 - i)\
+                   for rec_id, i in zip(range(0,3), range(0, 1080, 300))]
+
 
 READ_FROM_FILE = True
 
 if READ_FROM_FILE:
     filename='src/voice_recognition/nag3.wav'
+    filesize = os.path.getsize(filename)
  
     with open(filename, 'rb') as wav:
         header = wav.read(100)
+        byte_data = True
+        num_chunks = filesize // Settings.CHUNK_SIZE
         wav_data = wav.read()
 
 else:   
@@ -46,37 +61,61 @@ else:
 
 try:
     print("Recording audio... Press Ctrl+C to stop.")
-    wav_counter = 0
+    byte_counter = 0
     timestamp_error = 0
-    while(True):
-        if READ_FROM_FILE:
-            byte_data=wav_data[wav_counter*4*Settings.CHUNK_SIZE:(wav_counter+1)*4*Settings.CHUNK_SIZE]
-            wav_counter+=1
-        else:
-            byte_data = stream.read(Settings.CHUNK_SIZE)
+    if READ_FROM_FILE:
+        with tqdm(total=num_chunks) as pbar:
+            while(byte_data):
+                if READ_FROM_FILE:
+                    byte_data=wav_data[byte_counter*4*Settings.CHUNK_SIZE:(byte_counter+1)*4*Settings.CHUNK_SIZE]
+                    byte_counter += 1
+                    pbar.update(1)
+                else:
+                    byte_data = stream.read(Settings.CHUNK_SIZE)
 
-        sample = VoiceSample(byte_data)
-        sample.speech_probability =  vad(torch.from_numpy(sample.data_convert()), Settings.FREQUENCY).item()
+                sample = VoiceSample(byte_data)
+                sample.speech_probability =  vad(torch.from_numpy(sample.data_convert()), Settings.FREQUENCY).item()
 
-        #if sample.speech_probability <= 0.60:
-        #    print(f"speech probability: {sample.speech_probability}")
-        if sample.speech_probability > 0.60: 
-            recognizer.append_data(sample.mfcc_get().T, timestamp_error)
-            timestamp_error = 0
-            recognizer.train()
-            if recognizer.check_for_speaker_change():
-                print("speaker changed!")
-            recognizer.adjust()
-        else:
-            timestamp_error += Settings.SEGMENT_DURATION_MS / 1000
+                #if sample.speech_probability <= 0.60:
+                #    print(f"speech probability: {sample.speech_probability}")
+                if sample.speech_probability > 0.60: 
+                    for recognizer in recognizers:
+                        recognizer.append_data(sample.mfcc_get().T, timestamp_error)
+                        timestamp_error = 0
+                        recognizer.train()
+                        recognizer.adjust()
+                else:
+                    timestamp_error += Settings.SEGMENT_DURATION_MS / 1000
+    else:
+        while(byte_data):
+            if READ_FROM_FILE:
+                byte_data=wav_data[byte_counter*4*Settings.CHUNK_SIZE:(byte_counter+1)*4*Settings.CHUNK_SIZE]
+                byte_counter += 1
+            else:
+                byte_data = stream.read(Settings.CHUNK_SIZE)
 
+            sample = VoiceSample(byte_data)
+            sample.speech_probability =  vad(torch.from_numpy(sample.data_convert()), Settings.FREQUENCY).item()
+
+            #if sample.speech_probability <= 0.60:
+            #    print(f"speech probability: {sample.speech_probability}")
+            if sample.speech_probability > 0.60: 
+                for recognizer in recognizers:
+                    recognizer.append_data(sample.mfcc_get().T, timestamp_error)
+                    timestamp_error = 0
+                    recognizer.train()
+                    recognizer.adjust()
+            else:
+                timestamp_error += Settings.SEGMENT_DURATION_MS / 1000
                 
 except KeyboardInterrupt:
     print("Stopped recording")
 except IndexError:
-    print("Lubudubu")
+    print("IndexError")
 except ValueError:
-    print("lololololo")
+    print("ValueError - end of file")
 finally:
     if Settings.MAKE_PLOTS:
-        recognizer.plot()
+        for recognizer in recognizers:
+            recognizer.plot()
+        
