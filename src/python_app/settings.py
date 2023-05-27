38b6a0904b77c850bb4255cb30e9abfd8f17b121
Tomek Mycielski <60585367+Al-Gor1thm@users.noreply.github.com>
2023-05-27 18:03:17 +0200

    temporary vosk-based solution while I rewrite whisper " sliding window\

--- src/python_app/settings.py
+++ src/python_app/settings.py
@@ -3,23 +3,15 @@ import numpy
 
 
 class Settings:
-    STREAMFORMAT = pyaudio.paFloat32
+    STREAMFORMAT = pyaudio.paInt16
     DATAFORMAT = numpy.float32
     CHANNELS = 1  # Mono
     FREQUENCY = 48000  # Sampling rate (48kHz)
     SEGMENT_DURATION_MS = 100  # Segment duration in milliseconds
-    frames_per_segment = int(FREQUENCY * SEGMENT_DURATION_MS / 1000)
+    frames_per_segment = FREQUENCY * SEGMENT_DURATION_MS // 1000
     CHUNK_SIZE = frames_per_segment  # Size of each audio chunk
 
     # RECOGNIZER SETTINGS
     GMM_IS_TRAINED_DATA_TRESHOLD = 1620  # 16 seconds of training are enough
     NUMBER_TRESHOLD = 30  # 0
     PERCENTAGE_TRESHOLD = 0.35
-
-    # SPEECH TO TEXT SETTINGS
-    # model_choices=["tiny", "base", "small", "medium", "large"])
-    MODEL = 'medium'
-    LANGUAGE = 'pl'
-    ENERGY_THRESHOLD = 1000
-    RECORD_TIMEOUT = 2
-    PHRASE_TIMEOUT = 3
