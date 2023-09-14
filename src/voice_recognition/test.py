8cbd97c0df77e433acb2501ab41772dfadf329fd
cheatermen <elek123456789101112@gmail.com>
2023-09-14 16:39:51 +0200

    removed code for initializing Recognizer class with random parameters it wasn't very smart

--- src/voice_recognition/test.py
+++ src/voice_recognition/test.py
@@ -32,20 +32,6 @@ rn = np.random.RandomState(26)
 
 # Random Recongnizers
 recognizers = [Recongnizer(30,1)]
-#for i in range (Settings.N_OF_RECOGNIZERS):
-#    recognizers.append(Recongnizer(30, 0 + i, number_treshold= Settings.NUMBER_TRESHOLD + rn.randint(-20,20), percentage_treshold= Settings.PERCENTAGE_TRESHOLD + rn.uniform(-0.2,0.2), gmm_is_trained__data_treshold = Settings.GMM_IS_TRAINED_DATA_TRESHOLD - rn.randint(0,Settings.GMM_IS_TRAINED_DATA_TRESHOLD/2)))
-
-# Not random recongnizers
-
-#recognizers = [Recongnizer(30, 0 + rec_id, number_treshold=10 + i, percentage_treshold=0.20 + j, gmm_is_trained__data_treshold = 1600 - k)\
-#                for (rec_id, i, j, k) in zip(range(0, 10), np.arange(0, 30, 3), np.arange(0, 0.20, 0.02), range(0,  1180, 118))] \
-#+ [Recongnizer(30, 10 + rec_id, number_treshold=13 + i, percentage_treshold=0.35, gmm_is_trained__data_treshold = 1600)\
-#                   for rec_id, i in zip(range(0,10), np.arange(0, 30, 3))] \
-#+ [Recongnizer(30, 20 + rec_id, percentage_treshold=0.37 + i, gmm_is_trained__data_treshold = 1600)\
-#                   for rec_id, i in zip(range(0,10), np.arange(0, 0.20, 0.2))] \
-#+ [Recongnizer(30, 30 + rec_id, gmm_is_trained__data_treshold = 1320 - i)\
-#                   for rec_id, i in zip(range(0,3), range(0, 1080, 300))]
-
 
 READ_FROM_FILE = True
 
@@ -129,4 +115,3 @@ finally:
 if Settings.MAKE_PLOTS:
     for recognizer in recognizers:
         recognizer.plot()
-        
